2a5c1ab75568a040e053b09d52480a0a689cb27f
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-05-18 18:27:24 +0200

    Edit the function playRound() and the  comments

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,24 +14,24 @@
 // and reports a winner or looser at the end. 
 
 // Pseudocode:
-//  1- Write a function called getComputerChoice that will randomly return either 
+//  1- Write a function called getComputerChoice() that will randomly return either 
 //  "rock", "paper", or "scissors".
 
 //  2- Write a function that play a single round of Rock Paper Scissors:
 
-//      - The function called playRound should take 2 parameters 
-//      (playerSelection and computerSelection).
+//      - The function called playRound() should take 2 parameters 
+//      (playerSelection and computerSelection). ok
 
 //      -The function should return a string that declares the winner
-//      of the round like so: "You Lose! Paper beats Rock".
+//      of the round like so: "You Lose! Paper beats Rock". ok
 
-//      - The function function’s playerSelection parameter case-insensitive 
+//      - The function’s playerSelection parameter is case-insensitive 
 //      (so users can input rock, ROCK, RocK or any other variation).
 
 //      - I want to return the results of this function call, not console.log() 
 //      them; because I am going to use the return later on.
 
-//  3- Test the function playRound by using console.log() to see the results:
+//  3- Test the function playRound() by using console.log() to see the results:
 
         /* function playRound(playerSelection, computerSelection) {
             // your code here!
@@ -48,7 +48,9 @@
 //      - Use console.log() to display the result of each round and the winner at the end.
 //      - Use prompt to get input from the user.
 
-                /*         End of Plan      */
+                    /*         END OF PLAN     */
+                    
+                            /* STEP ONE */               
 
 //  1- Write a function called getComputerChoice that will randomly return either 
 //  "rock", "paper", or "scissors".
@@ -59,6 +61,66 @@ function getComputerChoice() {
     const getComputerChoice = choices[randomChoice];
     return getComputerChoice;
 }
+
+// Test the function getComputerChoice()
 //console.log(getComputerChoice());
 
+                /* TEST OK - END OF THE FIRST STEP */
 
+                        /* STEP TWO */
+
+//  2- Write a function that play a single round of Rock Paper Scissors:
+
+//      - The function playRound() takes 2 parameters (playerSelection, computerSelection)
+//      return a string that declares the winner
+//      of the round like so: "You Lose! Paper beats Rock".
+
+//      - The parameter playerSelection is case-insensitive via .toLowerCase
+//      (so users can input rock, ROCK, RocK or any other variation).
+
+//      - All of the results of this function are return, not console.log
+//      because I want to use them later on. 
+
+
+  function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return "It is a tie.";
+    } else {
+        switch (playerSelection.toLowerCase() !== computerSelection) {
+            
+            case (computerSelection === "rock"):
+                if (playerSelection.toLowerCase() === "scissors") {
+                    return "You lose! Rock breaks Scissors.";
+                } else if (playerSelection.toLowerCase() === "paper") {
+                    return "You win! Paper covers Rock.";
+                } break;
+            
+            case (computerSelection === "paper"):
+                if (playerSelection.toLowerCase() === "rock") {
+                    return "You lose! Paper covers Rock.";
+                } else if (playerSelection.toLowerCase() === "scissors") {
+                    return "You win! Scissors cuts Paper.";
+                } break;
+            
+            case (computerSelection === "scissors"):
+                if (playerSelection.toLowerCase() === "paper") {
+                    return "You lose! Scissors cuts Paper.";
+                } else if (playerSelection.toLowerCase() === "rock") {
+                    return "You win. Rock breaks Scissors.";
+                } break;
+            
+            default: return "I don't understand you!";
+        }
+    }
+}
+
+                    /* STEP THREE: TEST playRound() */
+// 3- Test ok for the function playRound by using console.log() to see the results:
+
+/*const playerSelection = "paper"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));*/
+
+/* STEP FOUR: TEST playRound() */
+// Write a NEW function call game()
