b9755220b365d5906e03480b024a6945ee4fdcd0
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-07-04 13:25:06 +0200

    - Remove the logic that plays 5 rounds
    - Create 3 buttons: one for each selection and add an event listener
    to the buttons that call playRound with the correct playerSelection
    every time a button is clicked.
    
    - Add a div for displaying the results and change all the console log
    into DOM methods.
    
    - Add 2 html files to be loaded once a player win the game.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,47 +1,27 @@
 
 // Pseudocode:
+
 //  1- Write a function called getComputerChoice() that will randomly return either 
 //  "rock", "paper", or "scissors". ok
 
 //  2- Write a function that play a single round of Rock Paper Scissors:
 
-//      - The function called playRound() should take 2 parameters 
-//      (playerSelection and computerSelection). ok
+//      - playRound() should take 2 parameters (playerSelection and computerSelection)
 
-//      -The function should return a string that declares the winner
+//      - Should return a string that declares the winner
 //      of the round like so: "You Lose! Paper beats Rock". ok
 
-//      - The function’s playerSelection parameter is case-insensitive 
-//      (so users can input rock, ROCK, RocK or any other variation).
+//      - Just return the results of this function call so we could use them later
 
-//      - I want to return the results of this function call, not console.log() 
-//      them; because I am going to use the return later on.
+//  3- Write a NEW function called game(). Use the previous function {inside} of this one 
+//  to display the running score and announce the winner once a player reaches 5 points.
 
-//  3- Test the function playRound() by using console.log() to see the results:
 
-        /* function playRound(playerSelection, computerSelection) {
-            // your code here!
-            }
- 
-            const playerSelection = "rock";
-            const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection)); */
 
-//  4- Write a NEW function called game().Use the previous function {inside} of this one 
-//  to play a 5 round game that keeps score and reports a winner or loser at the end.
-
-//      - Call playRound function 5 times in a row (or use a loop).
-//      - Use console.log() to display the result of each round and the winner at the end.
-//      - Use prompt to get input from the user.
-
-  
-//playerScore and computerScore to keep count of the score:
+// Game:
 
 let computerScore = 0;
 let playerScore = 0;
-
-//  1- getComputerChoice() will randomly return either 
-//  "rock", "paper", or "scissors".
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -50,91 +30,84 @@ function getComputerChoice() {
     return getComputerChoice;
 }
 
-// Test the function getComputerChoice()
-//console.log(getComputerChoice()); (test ok)
+function playRound(playerSelection, computerSelection) {
+  
+  if (playerSelection === computerSelection) {
+      return `Tie game.`;    
+  } else {
+      switch (playerSelection !== computerSelection) {
 
-//  2- A single round of Rock Paper Scissors:
-
-//      - playRound() takes 2 parameters (playerSelection, computerSelection)
-//      return a string that declares the winner of the round like so: 
-//      "You Lose! Paper beats Rock".
-
-//      - playerSelection is case-insensitive via .toLowerCase
-//      (so users can input rock, ROCK, RocK or any other variation).
-
-//      - All of the results of this function are return, not console.log
-//      because I want to use them later on.
-
-//     - Increment the playerScore and computerScore variables 
-//       if the player or the computer wins the round.
-
-
-  function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection.toLowerCase() === computerSelection) {
-        return "It is a tie.";
-    } else {
-        switch (playerSelection.toLowerCase() !== computerSelection) {
-            
-            case (computerSelection === "rock"):
-                if (playerSelection.toLowerCase() === "scissors") {
-                    computerScore += 1;
-                    return `You lose! ${computerSelection} breaks ${playerSelection}!` ;
-                } else if (playerSelection.toLowerCase() === "paper") {
-                    playerScore += 1;
-                    return `You win! ${playerSelection} covers ${computerSelection}!`;
-                } break;
-            
-            case (computerSelection === "paper"):
-                if (playerSelection.toLowerCase() === "rock") {
-                    computerScore += 1;
-                    return `You lose! ${computerSelection} covers ${playerSelection}!` ;
-                } else if (playerSelection.toLowerCase() === "scissors") {
-                    playerScore += 1;
-                    return `You win! ${playerSelection} cuts ${computerSelection}!` ;
-                } break;
-            
-            case (computerSelection === "scissors"):
-                if (playerSelection.toLowerCase() === "paper") {
-                    computerScore += 1;
-                    return `You lose! ${computerSelection} cuts ${playerSelection}!` ;
-                } else if (playerSelection.toLowerCase() === "rock") {
-                    playerScore += 1;
-                    return `You win! ${playerSelection} breaks ${computerSelection}!` ;
-                } break;
-            
-            default: return "I don't understand you!";
-        }
-    }
+          case (computerSelection === "rock"):
+              if (playerSelection === "scissors") {
+                  computerScore += 1;
+                  return `You lose! ${computerSelection} breaks ${playerSelection}!`;
+              } else if (playerSelection === "paper") {
+                  playerScore += 1;
+                  return `You win! ${playerSelection} covers ${computerSelection}!`;
+              } break;
+          
+          case (computerSelection === "paper"):
+              if (playerSelection === "rock") {
+                  computerScore += 1;
+                  return `You lose! ${computerSelection} covers ${playerSelection}!`;                
+              } else if (playerSelection === "scissors") {
+                  playerScore += 1;
+                  return `You win! ${playerSelection} cuts ${computerSelection}!`;
+              } break;
+          
+          case (computerSelection === "scissors"):
+              if (playerSelection === "paper") {
+                  computerScore += 1;
+                  return `You lose! ${computerSelection} cuts ${playerSelection}!`;
+              } else if (playerSelection === "rock") {
+                  playerScore += 1;
+                  return `You win! ${playerSelection} breaks ${computerSelection}!`;                    
+              } break;
+          
+          default: if (playerSelection === "undefined") {
+              return "Please, click a button.";
+          }
+      }
   }
-                    
-// 3- Test ok for the function playRound by using console.log() to see the results:
-
-/*const playerSelection = "paper"
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));*/
-
-// 4- Within game(): playRound() is called 5 times in a row inside the loop.   
-//  - prompt() is used to get input from the player and console.log() to display
-//    the winner at the end of each round.
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-
-    let playerSelection = prompt("Rock, Paper or Scissors...? Choose.");
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    }
-
-    if (playerScore > computerScore) {
-        return console.log(`Player score is: ${playerScore}. Computer score is: ${computerScore}. You win!`);
-        } else if (computerScore > playerScore) {
-        return console.log(`Player score is: ${playerScore}. Computer score is: ${computerScore}. You lose`);
-    } else {
-        return console.log(`Player score is: ${playerScore}. Computer score is: ${computerScore}. Tie game!`);
-    }
-    
 }
-game();
 
+// UI
 
+function game(e) {
+    
+    const playerScorePara = document.querySelector('#playerScore');
+    const computerScorePara = document.querySelector('#computerScore');
+    const scoreMessage = document.querySelector('#scoreMessage')
+    
+    playerSelection = e.target.textContent;
+    computerSelection = getComputerChoice();
+    scoreMessage.textContent = `${playRound(playerSelection, computerSelection)}`;
+    playerScorePara.textContent = `Player: ${playerScore}`;
+    computerScorePara.textContent = `Computer: ${computerScore}`;
+    
+    // When a player scores 5 load new page with a button to play again. 
+    
+    if (playerScore === 5 || computerScore === 5) {
+        
+        scoreMessage.textContent = `${winGame()}`;
+        if (playerScore > computerScore) {
+            setTimeout(() => window.location.href = 'winRestart.html', 1000);
+            // window.location.href = 'winRestart.html';
+        } else {
+            setTimeout(() => window.location.href = 'lostRestart.html', 1000);
+            // window.location.href = 'lostRestart.html';
+        }      
+    }
+}
+
+function winGame() {
+    if (playerScore > computerScore) {
+        return `Player score is: ${playerScore}. Computer score is: ${computerScore}. You win!`;
+    } else if (computerScore > playerScore) {
+        return `Player score is: ${playerScore}. Computer score is: ${computerScore}. You lose`;
+    } else {
+        return `Player score is: ${playerScore}. Computer score is: ${computerScore}. Tie game!`;
+    }
+}
+
+window.addEventListener('click', game);
